bce5ca15f1ac9a533405528c18e27c67d60e8ef5
Ben <benjamin.schoenburg@madvertise.com>
2015-03-04 18:24:44 +0100

    clean up tests

--- tests/unit/copyable-test.js
+++ tests/unit/copyable-test.js
@@ -8,57 +8,23 @@ import startApp from '../helpers/start-app';
 var store;
 var app;
 
-module('copyable - synchronous relations', {
+module('copyable - synchronous shallow relations', {
   beforeEach: function() {
-
     app = startApp();
+    store = app.__container__.lookup('store:main');
 
     app.Foo = DS.Model.extend( {
-      prop1: DS.attr('string'),
-      prop2: DS.attr('string')
+      prop: DS.attr('string')
     });
 
     app.Bar = DS.Model.extend( Copyable, {
-      foo: DS.belongsTo('foo'),
+      foo: DS.belongsTo('foo')
     });
 
     app.Baz = DS.Model.extend( Copyable, {
-      foos: DS.hasMany('foo'),
+      foos: DS.hasMany('foo')
     });
-
-    app.Fee = DS.Model.extend( Copyable, {
-      prop1: DS.attr('string'),
-      prop2: DS.attr('string')
-    });
-
-    app.Ber = DS.Model.extend( Copyable, {
-      fee: DS.belongsTo('fee'),
-    });
-
-    app.Bez = DS.Model.extend( Copyable, {
-      fees: DS.hasMany('fee'),
-    });
-
-    store = app.__container__.lookup('store:main');
   }
-});
-
-test('it copies attributes', function(assert) {
-  assert.expect(2);
-
-  Ember.run(function() {
-
-    var fee = store.createRecord('fee', {
-      prop1: 'prop1',
-      prop2: 'prop2'
-    });
-
-    fee.copy().then(function(copy) {
-
-      assert.equal(copy.get('prop1'), fee.get('prop1'));
-      assert.equal(copy.get('prop2'), fee.get('prop2'));
-    });
-  });
 });
 
 test('it shallow copies belongsTo relations', function(assert) {
@@ -99,47 +65,85 @@ test('it shallow copies hasMany relations', function(assert) {
   });
 });
 
+module('copyable - synchronous deep relations', {
+  beforeEach: function() {
+
+    app = startApp();
+
+    app.Foo = DS.Model.extend( Copyable, {
+      prop: DS.attr('string')
+    });
+
+    app.Bar = DS.Model.extend( Copyable, {
+      foo: DS.belongsTo('foo')
+    });
+
+    app.Baz = DS.Model.extend( Copyable, {
+      foos: DS.hasMany('foo')
+    });
+
+    store = app.__container__.lookup('store:main');
+  }
+});
+
+test('it copies attributes', function(assert) {
+  assert.expect(2);
+
+  Ember.run(function() {
+
+    var foo = store.createRecord('foo', {
+      prop: 'prop',
+    });
+
+    foo.set('id', 1);
+    foo.copy().then(function(copy) {
+
+      assert.notEqual(copy.get('id'), foo.get('id'));
+      assert.equal(copy.get('prop'), foo.get('prop'));
+    });
+  });
+});
+
 test('it deep copies belongsTo relations', function(assert) {
   assert.expect(2);
 
   Ember.run(function() {
 
-    var fee = store.createRecord('fee');
-    fee.set('prop1', 'prop1');
-    fee.set('id', 1);
+    var foo = store.createRecord('foo');
+    foo.set('prop', 'prop');
+    foo.set('id', 1);
 
-    var ber = store.createRecord('ber', {
-      fee: fee
+    var bar = store.createRecord('bar', {
+      foo: foo
     });
 
-    ber.copy().then(function(copy) {
-      assert.notEqual(copy.get('fee.id'), ber.get('fee.id'));
-      assert.equal(copy.get('fee.prop1'), ber.get('fee.prop1'));
+    bar.copy().then(function(copy) {
+      assert.notEqual(copy.get('foo.id'), bar.get('foo.id'));
+      assert.equal(copy.get('foo.prop'), bar.get('foo.prop'));
     });
   });
 });
-
 
 test('it deep copies hasMany relations', function(assert) {
   assert.expect(4);
 
   Ember.run(function() {
 
-    var fee1 = store.createRecord('fee');
-    var fee2 = store.createRecord('fee');
-    var bez = store.createRecord('bez');
+    var foo1 = store.createRecord('foo');
+    var foo2 = store.createRecord('foo');
+    var baz = store.createRecord('baz');
 
-    fee1.set('id', 1);
-    fee2.set('id', 2);
-    fee1.set('prop1', 'propA');
-    fee2.set('prop1', 'propB');
-    bez.get('fees').pushObjects([fee1,fee2]);
+    foo1.set('id', 1);
+    foo2.set('id', 2);
+    foo1.set('prop', 'prop1');
+    foo2.set('prop', 'prop2');
+    baz.get('foos').pushObjects([foo1,foo2]);
 
-    bez.copy().then(function(copy) {
-      assert.notEqual(copy.get('fees.firstObject.id'), bez.get('fees.firstObject.id'));
-      assert.equal(copy.get('fees.firstObject.prop1'), bez.get('fees.firstObject.prop1'));
-      assert.notEqual(copy.get('fees.lastObject.id'), bez.get('fees.lastObject.id'));
-      assert.equal(copy.get('fees.lastObject.prop1'), bez.get('fees.lastObject.prop1'));
+    baz.copy().then(function(copy) {
+      assert.notEqual(copy.get('foos.firstObject.id'), baz.get('foos.firstObject.id'));
+      assert.equal(copy.get('foos.firstObject.prop'), baz.get('foos.firstObject.prop'));
+      assert.notEqual(copy.get('foos.lastObject.id'), baz.get('foos.lastObject.id'));
+      assert.equal(copy.get('foos.lastObject.prop'), baz.get('foos.lastObject.prop'));
     });
   });
 });
