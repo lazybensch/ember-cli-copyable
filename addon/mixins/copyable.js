faaf45842787607f246ef3efb2e1b840d1639bf7
Ben <benjamin.schoenburg@madvertise.com>
2015-03-06 09:03:44 +0100

    use ember data relationship api

--- addon/mixins/copyable.js
+++ addon/mixins/copyable.js
@@ -9,94 +9,89 @@ var Copyable = Ember.Mixin.create({
     var _this = this;
     return new Ember.RSVP.Promise(function(resolve) {
 
-      var type = _this.constructor.typeKey;
-      var attributes = Ember.get(_this.constructor, 'attributes').keys.list;
-      var relationships = _this._relationships;
-      var copy = _this.get('store').createRecord(type);
+      var model = _this.constructor;
+      var copy = _this.get('store').createRecord(model.typeKey);
       var queue = [];
 
-      if (attributes.length) {
-        attributes.forEach(function (attr) {
-          copy.set(attr, _this.get(attr));
-        });
-      }
+      model.eachAttribute(function(attr) {
+        copy.set(attr, _this.get(attr));
+      });
 
-      if (Ember.keys(relationships).length) {
-        Ember.keys(relationships).forEach(function (rel) {
+      model.eachRelationship(function(rel, meta) {
+        console.log(_this.get(rel));
+        if (Ember.none(_this.get(rel))) {
+          return;
+        }
 
-          if (Ember.none(_this.get(rel))) {
-            return;
-          }
+        if (_this.get(rel).constructor === DS.PromiseObject) {
 
-          if (_this.get(rel).constructor === DS.PromiseObject) {
+          queue.pushObject(_this.get(rel).then(function(obj) {
 
-            queue.pushObject(_this.get(rel).then(function(obj) {
-
-              if (obj.get('copyable')) {
-                return obj.copy().then(function(objCopy) {
-                  copy.set(rel, objCopy);
-                });
-
-              } else {
-                copy.set(rel, obj);
-              }
-
-            }));
-
-
-          } else if (_this.get(rel).constructor === DS.PromiseManyArray) {
-
-            queue.pushObject(_this.get(rel).then(function(array) {
-
-              array.forEach(function(obj) {
-                if (obj.get('copyable')) {
-                  return obj.copy().then(function(objCopy) {
-                    copy.get(rel).pushObject(objCopy);
-                  });
-
-                } else {
-                  copy.get(rel).pushObject(obj);
-                }
+            if (obj.get('copyable')) {
+              return obj.copy().then(function(objCopy) {
+                copy.set(rel, objCopy);
               });
 
-
-            }));
-
-          } else {
-
-            if (relationships[rel].relationshipMeta.kind === 'belongsTo') {
-              var obj = _this.get(rel);
-
-              if (obj.get('copyable')) {
-                queue.pushObject( obj.copy().then(function(objCopy) {
-                  copy.set(rel, objCopy);
-                }));
-
-              } else {
-                copy.set(rel, obj);
-              }
-
             } else {
-              var objs = _this.get(rel);
-
-              if (objs.get('firstObject.copyable')) {
-
-                var copies = objs.map(function(obj) {
-                  return obj.copy();
-                });
-
-                queue.pushObject( Ember.RSVP.all(copies).then( function(resolvedCopies) {
-                  copy.get(rel).pushObjects(resolvedCopies);
-                }));
-
-              } else {
-                copy.get(rel).pushObjects(objs);
-              }
+              copy.set(rel, obj);
             }
 
+          }));
+
+
+        } else if (_this.get(rel).constructor === DS.PromiseManyArray) {
+
+          queue.pushObject(_this.get(rel).then(function(array) {
+
+            array.forEach(function(obj) {
+              if (obj.get('copyable')) {
+                return obj.copy().then(function(objCopy) {
+                  copy.get(rel).pushObject(objCopy);
+                });
+
+              } else {
+                copy.get(rel).pushObject(obj);
+              }
+            });
+
+
+          }));
+
+        } else {
+
+          if (meta.kind === 'belongsTo') {
+            var obj = _this.get(rel);
+
+            if (obj.get('copyable')) {
+              queue.pushObject( obj.copy().then(function(objCopy) {
+                copy.set(rel, objCopy);
+              }));
+
+            } else {
+              copy.set(rel, obj);
+            }
+
+          } else {
+            var objs = _this.get(rel);
+
+            if (objs.get('firstObject.copyable')) {
+
+              var copies = objs.map(function(obj) {
+                return obj.copy();
+              });
+
+              queue.pushObject( Ember.RSVP.all(copies).then( function(resolvedCopies) {
+                copy.get(rel).pushObjects(resolvedCopies);
+              }));
+
+            } else {
+              copy.get(rel).pushObjects(objs);
+            }
           }
-        });
-      }
+
+        }
+      });
+
 
       Ember.RSVP.all(queue).then(function() {
         resolve(copy);
