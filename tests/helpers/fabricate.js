d73ff4315ed43cab4b641be83d3f2d6419a02340
Ben <benjamin.schoenburg@madvertise.com>
2015-03-06 09:03:21 +0100

    extract fabrication helper

---
+++ tests/helpers/fabricate.js
@@ -0,0 +1,102 @@
+import DS from 'ember-data';
+import Copyable from 'ember-cli-copyable';
+
+var setupModels = function(app, async) {
+  app.Foo = DS.Model.extend( Copyable, {
+    property: DS.attr('string')
+  });
+
+  app.Bar = DS.Model.extend( Copyable, {
+    foo: DS.belongsTo('foo', { async: async })
+  });
+
+  app.Baz = DS.Model.extend( Copyable, {
+    foos: DS.hasMany('foo', {async: async })
+  });
+
+  app.Multi = DS.Model.extend( Copyable, {
+    bars: DS.hasMany('bar', {async: async }),
+    baz: DS.belongsTo('baz', {async: async })
+  });
+
+  app.FooFix = DS.Model.extend( {
+    property: DS.attr('string')
+  });
+
+  app.FooBar = DS.Model.extend( Copyable, {
+    fooFix: DS.belongsTo('fooFix', { async: async })
+  });
+};
+
+var setupFixtures = function(app) {
+  app.Foo.reopenClass({
+    FIXTURES: [
+      {
+        'id': '1',
+        'property': 'prop1'
+      },
+      {
+        'id': '2',
+        'property': 'prop2'
+      },
+      {
+        'id': '3',
+        'property': 'prop3'
+      }
+    ]
+  });
+
+  app.Bar.reopenClass({
+    FIXTURES: [
+      {
+        'id': '1',
+        'foo': '1'
+      }
+    ]
+  });
+
+  app.Baz.reopenClass({
+    FIXTURES: [
+      {
+        'id': '1',
+        'foos': ['1', '2']
+      }
+    ]
+  });
+
+  app.Multi.reopenClass({
+    FIXTURES: [
+      {
+        'id': '1',
+        'bars': ['1'],
+        'baz': '1'
+      }
+    ]
+  });
+
+  app.FooFix.reopenClass({
+    FIXTURES: [
+      {
+        'id': '1',
+        'property': 'fix1'
+      }
+    ]
+  });
+
+  app.FooBar.reopenClass({
+    FIXTURES: [
+      {
+        'id': '1',
+        'fooFix': '1'
+      }
+    ]
+  });
+};
+
+export default function fabricate(app, async) {
+
+  setupModels(app, async);
+  setupFixtures(app);
+  app.ApplicationAdapter = DS.FixtureAdapter;
+  return app.__container__.lookup('store:main');
+}
--- tests/unit/async-copying-test.js
+++ tests/unit/async-copying-test.js
@@ -1,108 +1,14 @@
 import Ember from 'ember';
-import Copyable from 'ember-cli-copyable';
-import DS from 'ember-data';
 import { test } from 'ember-qunit';
 import { module } from 'qunit';
 import startApp from '../helpers/start-app';
+import fabricate from '../helpers/fabricate';
 
 var store;
 
-var fabricate = function(app, async) {
-  app.Foo = DS.Model.extend( Copyable, {
-    property: DS.attr('string')
-  });
-
-  app.Bar = DS.Model.extend( Copyable, {
-    foo: DS.belongsTo('foo', { async: async })
-  });
-
-  app.Baz = DS.Model.extend( Copyable, {
-    foos: DS.hasMany('foo', {async: async })
-  });
-
-  app.Multi = DS.Model.extend( Copyable, {
-    bars: DS.hasMany('bar', {async: async }),
-    baz: DS.belongsTo('baz', {async: async })
-  });
-
-  app.FooFix = DS.Model.extend( {
-    property: DS.attr('string')
-  });
-
-  app.FooBar = DS.Model.extend( Copyable, {
-    fooFix: DS.belongsTo('fooFix', { async: async })
-  });
-
-  app.Foo.reopenClass({
-    FIXTURES: [
-      {
-        'id': '1',
-        'property': 'prop1'
-      },
-      {
-        'id': '2',
-        'property': 'prop2'
-      },
-      {
-        'id': '3',
-        'property': 'prop3'
-      }
-    ]
-  });
-
-  app.Bar.reopenClass({
-    FIXTURES: [
-      {
-        'id': '1',
-        'foo': '1'
-      }
-    ]
-  });
-
-  app.Baz.reopenClass({
-    FIXTURES: [
-      {
-        'id': '1',
-        'foos': ['1', '2']
-      }
-    ]
-  });
-
-  app.Multi.reopenClass({
-    FIXTURES: [
-      {
-        'id': '1',
-        'bars': ['1'],
-        'baz': '1'
-      }
-    ]
-  });
-
-  app.FooFix.reopenClass({
-    FIXTURES: [
-      {
-        'id': '1',
-        'property': 'fix1'
-      }
-    ]
-  });
-
-  app.FooBar.reopenClass({
-    FIXTURES: [
-      {
-        'id': '1',
-        'fooFix': '1'
-      }
-    ]
-  });
-
-  app.ApplicationAdapter = DS.FixtureAdapter;
-  store = app.__container__.lookup('store:main');
-};
-
 module('async copying', {
   beforeEach: function() {
-    fabricate(startApp(), true);
+    store = fabricate(startApp(), true);
   }
 });
 
@@ -170,7 +76,7 @@ test('it copies complex objects', function(assert) {
 
 module('sync copying', {
   beforeEach: function() {
-    fabricate(startApp(), false);
+    store = fabricate(startApp(), false);
 
     return Ember.RSVP.all(['foo','bar','baz','multi','fooBar','fooFix'].map(function(type) {
       return store.find(type);
